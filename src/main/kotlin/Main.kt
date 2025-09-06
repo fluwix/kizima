0ff6db34b41db7a317a3918b46a64f46574ef30c
kizima <kable660@gmail.com>
2025-09-06 11:20:00 +0300

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,35 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    println("Введите последовательность символов.")
+    val input = readLine()!!.toUpperCase() // Преобразуем в верхний регистр
+    if (input.isEmpty()) {
+        println("Ничего не введено.")
+        return
+    }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val result = StringBuilder()
+    var currentChar = input[0]
+    var count = 1
+
+    for (i in 1 until input.length) {
+        val char = input[i]
+        if (char == currentChar) {
+            count++
+        } else {
+            if (count > 1) {
+                result.append("$currentChar$count")
+            } else {
+                result.append(currentChar)
+            }
+            currentChar = char
+            count = 1
+        }
+    }
+
+    if (count > 1) {
+        result.append("$currentChar$count")
+    } else {
+        result.append(currentChar)
+    }
+
+    println(result.toString())
 }
